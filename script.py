ab2811dd6c5f5f5112fc1ae1b50b2f4740f7c288
Adama SAMAKE <as91404togo@gmail.com>
2024-05-06 20:59:06 +0000

    Recupération de toutes les valeurs importantes d'une seule société (pas nettoyées)

--- script.py
+++ script.py
@@ -1,10 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = 'https://cfetogo.tg/annonces-legales/details-annonce-1.html'
 response = requests.get(url)
 
-soup = BeautifulSoup(response.content, 'html.parser')
+company = {}
 
-company_title = soup.find('div', {'class': 'main'}).find_all('p')[2]
-print(company_title)
+soup = BeautifulSoup(response.content, 'html.parser')
+main_page = soup.find('div', {'class': 'main'})
+
+#company_title = main_page.find_all('p')[2]
+#company['name'] = company_title.text
+
+p_list = main_page.find_all('p')
+for p in p_list:
+    for expr in [r'\ADENOMINATION', r'\AOBJET SOCIAL', r'\ACAPITAL SOCIAL', r'\ASIEGE SOCIAL', r'\ADUREE', r'\AGERANCE']:
+        res = re.search(expr, p.text)
+        if res != None:
+            company[expr[2:]] = res.string.split(':')[1]
+            continue
+
+#for (k,v) in company.items():
+#    print(k, '->', v)
+
+print(company["DENOMINATION"])
