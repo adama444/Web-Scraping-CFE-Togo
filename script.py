26de32707d12798a3baf08354c95efd372bc0527
Adama SAMAKE <as91404togo@gmail.com>
2024-05-08 00:56:35 +0000

    Clean the code for scraping one company data and export it to csv

--- script.py
+++ script.py
@@ -1,23 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import csv
 
 url = 'https://cfetogo.tg/annonces-legales/details-annonce-2.html'
 response = requests.get(url)
-
-company = {}
-
 soup = BeautifulSoup(response.content, 'html.parser')
 main_page = soup.find('div', {'class': 'main'})
 
+company = {}
+
+sector = {'is_import_export': r'import[ ]*-[ ]*export', 'is_transport': 'transport', 'is_real_estate': 'immobilier', 'is_agrobusiness': 'agrobusiness', 'is_restoration': 'restauration', 'is_investment': 'investissement', 'is_general_commerce': r'commerce g.n.ral', 'is_BTP': r'travaux [a-z]+ b.timent'}
+
 email_pattern = r'[a-zA-Z0-9]+[-._]*[a-zA-Z0-9]+@[a-zA-Z0-9-]+\.[a-z]{2,}'
 phone_number_pattern = r'([0-9]{8,13})|(\d{2} \d{2} \d{2} \d{2})'
+manager_pattern = re.compile(r'\b(mademoiselle|monsieur)\b [a-zA-Z]+( [a-zA-Z]+)*', re.IGNORECASE)
 
-emails = set(re.findall(email_pattern, main_page.get_text()))
-if emails != set():
-    company['email'] = [v for v in emails][0]
+p_list = main_page.find_all('p')
+for p in p_list:
+    for expr in [r'\Adenomination', r'\Asiege social', r'\Ageranc']:
+        res = re.search(expr, p.get_text().lower())
+        if res != None:
+            company[expr[2:]] = res.string.split(':')[1]
+            continue
+
+company["name"] = str(company["denomination"]).replace('«', '').replace('»', '').strip()
+
+company["capital"] = re.search(r'\d+[.]*[ ]*\d{3}[.]*[ ]*\d{3}', main_page.getText()).group().replace(' ', '').replace('.', '')
+
+company['manager'] = re.search(manager_pattern, company['geranc']).group().upper()
+
+if re.search('[lom]|[togo]', company['siege social']) != None:
+    company["is_national"] = True
 else:
-    company['email'] = ''
+    company["is_national"] = False
 
 phone_numbers = re.findall(phone_number_pattern, main_page.get_text())
 formatted_numbers = set()
@@ -29,28 +45,27 @@ for phone_list in phone_numbers:
 if formatted_numbers != set():
     company['phone_number'] = [v for v in formatted_numbers][0]
 else:
-    company['phone_number'] = ''
+    company['phone_number'] = '-'
 
-
-p_list = main_page.find_all('p')
-for p in p_list:
-    for expr in [r'\Adenomination', r'\Aobjet', r'\Acapital social', r'\Asiege social', r'\Ageranc']:
-        res = re.search(expr, p.get_text().lower())
-        if res != None:
-            company[expr[2:]] = res.string.split(':')[1]
-            continue
-
-company["name"] = str(company["denomination"]).lower().replace('«', '').replace('»', '').strip()
-
-company["capital"] = re.search(r'\d+[.]*[ ]*\d{3}[.]*[ ]*\d{3}', main_page.getText()).group().replace(' ', '').replace('.', '')
-
-if re.search('[lom]|[togo]', company['siege social']) != None:
-    company["is_national"] = True
+emails = set(re.findall(email_pattern, main_page.get_text()))
+if emails != set():
+    company['email'] = [v for v in emails][0]
 else:
-    company["is_national"] = False
+    company['email'] = '-'
 
-manager_pattern = re.compile(r'\b(mademoiselle|monsieur)\b [a-zA-Z]+( [a-zA-Z]+)*', re.IGNORECASE)
-company['manager'] = re.search(manager_pattern, company['geranc']).group().upper()
+for (k,v) in sector.items():
+    if re.search(v, main_page.get_text().lower()) != None:
+        company[k] = True
+    else:
+        company[k] = False
 
+company.pop('denomination')
+company.pop('siege social')
+company.pop('geranc')
 for (k,v) in company.items():
     print(k, '->', v)
+
+with open("company.csv", "w") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=company.keys())
+    writer.writeheader()
+    writer.writerows([company])
